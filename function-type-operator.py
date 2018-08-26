de359fa3dd0cd1438cb4a88681b3b77181d87bbb
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-08-27 05:30:10 +0900

    Learnd about function, type, operator

---
+++ function-type-operator.py
@@ -0,0 +1,28 @@
+# Output
+print('Hello, world!')
+print('\n')
+
+# Various types
+print(3 + 2) # int
+print(3.0 + 2.0) # float
+print(3 != 2) # bool
+print('\n')
+
+# Output type
+print(type(3)) # int
+print(type(3.0)) # float
+print('\n')
+
+# Bools
+print(True and True) # >>> True
+print(True and False) # >>> False
+print(True or False) # >>> True
+print(False or False) # >>> False
+print(not False) # >>> True
+print(not True) # >>> False
+print('\n')
+
+# Power operator
+print(3**2) # 3 to the 2nd 
+print(2**(2**2)) # 2^(2^2) = 2^4 = 16
+print('\n')
