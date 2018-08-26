624a602a0d5b90637664af36cdd8163818c2f191
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-08-27 05:33:01 +0900

    Learned variable declarement

---
+++ variable.py
@@ -0,0 +1,15 @@
+# Basic declarement
+x = "x"
+print('x = ' + x) # >>> x = x
+print('\n')
+
+# Mulch declarement
+x, y = "x", "y"
+print('x = ' + x + ', y = ' + y) # >>> x = x, y = y
+
+# swap
+x, y = y, x
+print('x = ' + x + ', y = ' + y) # >>> x = y, y = x
+print('\n')
+
+
